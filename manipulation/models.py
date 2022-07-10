850dd655390ccb426443ba0d4fcaea91cae22998
Xurvan <xurvan@tutanota.com>
2022-07-11 01:11:19 +0300

    Add "ManipulationMixinModel"

---
+++ manipulation/models.py
@@ -0,0 +1,15 @@
+from django.db.models import DateTimeField
+
+
+class ManipulationMixinModel:
+    inserted_at = DateTimeField(
+        auto_now_add=True,
+        db_index=True,
+    )
+    updated_at = DateTimeField(
+        auto_now=True,
+        db_index=True,
+    )
+
+    class Meta:
+        abstract = True
